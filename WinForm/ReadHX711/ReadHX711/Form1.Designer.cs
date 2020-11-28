6d6ec0c5b9d4562ec16a77a5eac865994b741706
danadeutsch <deutschdana@gmail.com>
2020-11-27 21:45:43 -0800

    full cycle works recorded

--- WinForm/ReadHX711/ReadHX711/Form1.Designer.cs
+++ WinForm/ReadHX711/ReadHX711/Form1.Designer.cs
@@ -32,10 +32,10 @@
             System.Windows.Forms.Label label6;
             System.Windows.Forms.Label label1;
             System.Windows.Forms.Label label2;
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.Label label4;
             System.Windows.Forms.Label label5;
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.txtBaud = new System.Windows.Forms.TextBox();
             this.cmbPorts = new System.Windows.Forms.ComboBox();
@@ -64,9 +64,10 @@
             // 
             label6.AutoSize = true;
             label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            label6.Location = new System.Drawing.Point(206, 40);
+            label6.Location = new System.Drawing.Point(412, 77);
+            label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             label6.Name = "label6";
-            label6.Size = new System.Drawing.Size(119, 16);
+            label6.Size = new System.Drawing.Size(233, 30);
             label6.TabIndex = 26;
             label6.Text = "Current ADC Value";
             // 
@@ -74,9 +75,10 @@
             // 
             label1.AutoSize = true;
             label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            label1.Location = new System.Drawing.Point(207, 83);
+            label1.Location = new System.Drawing.Point(414, 160);
+            label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(126, 16);
+            label1.Size = new System.Drawing.Size(244, 30);
             label1.TabIndex = 28;
             label1.Text = "Current Span In Bed";
             label1.Click += new System.EventHandler(this.label1_Click);
@@ -85,53 +87,79 @@
             // 
             label2.AutoSize = true;
             label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            label2.Location = new System.Drawing.Point(306, 5);
+            label2.Location = new System.Drawing.Point(612, 10);
+            label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             label2.Name = "label2";
-            label2.Size = new System.Drawing.Size(96, 16);
+            label2.Size = new System.Drawing.Size(180, 30);
             label2.TabIndex = 30;
             label2.Text = "Bytes to Read:";
             // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label4.Location = new System.Drawing.Point(724, 96);
+            label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(210, 37);
+            label4.TabIndex = 34;
+            label4.Text = "Amount Slept";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label5.Location = new System.Drawing.Point(724, 165);
+            label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(296, 37);
+            label5.TabIndex = 36;
+            label5.Text = "Amount Waking Up";
+            // 
             // chart1
             // 
-            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            this.chart1.Location = new System.Drawing.Point(12, 135);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(24, 260);
+            this.chart1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series2.Name = "hx711";
-            this.chart1.Series.Add(series2);
-            this.chart1.Size = new System.Drawing.Size(634, 365);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Name = "hx711";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1268, 685);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
             // 
             // txtBaud
             // 
             this.txtBaud.BackColor = System.Drawing.SystemColors.Menu;
-            this.txtBaud.Location = new System.Drawing.Point(220, 12);
+            this.txtBaud.Location = new System.Drawing.Point(440, 23);
+            this.txtBaud.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtBaud.Name = "txtBaud";
-            this.txtBaud.Size = new System.Drawing.Size(68, 20);
+            this.txtBaud.Size = new System.Drawing.Size(132, 31);
             this.txtBaud.TabIndex = 13;
             this.txtBaud.Text = "9600";
             // 
             // cmbPorts
             // 
             this.cmbPorts.FormattingEnabled = true;
-            this.cmbPorts.Location = new System.Drawing.Point(93, 12);
+            this.cmbPorts.Location = new System.Drawing.Point(186, 23);
+            this.cmbPorts.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.cmbPorts.Name = "cmbPorts";
-            this.cmbPorts.Size = new System.Drawing.Size(121, 21);
+            this.cmbPorts.Size = new System.Drawing.Size(238, 33);
             this.cmbPorts.TabIndex = 12;
             this.cmbPorts.SelectedIndexChanged += new System.EventHandler(this.cmbPorts_SelectedIndexChanged);
             this.cmbPorts.MouseClick += new System.Windows.Forms.MouseEventHandler(this.cmbPorts_MouseClick);
             // 
             // btnConnect
             // 
-            this.btnConnect.Location = new System.Drawing.Point(12, 12);
+            this.btnConnect.Location = new System.Drawing.Point(24, 23);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.Size = new System.Drawing.Size(150, 44);
             this.btnConnect.TabIndex = 11;
             this.btnConnect.Text = "Connect";
             this.btnConnect.UseVisualStyleBackColor = true;
@@ -149,17 +177,19 @@
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.SystemColors.MenuBar;
-            this.textBox1.Location = new System.Drawing.Point(209, 59);
+            this.textBox1.Location = new System.Drawing.Point(418, 113);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(116, 20);
+            this.textBox1.Size = new System.Drawing.Size(228, 31);
             this.textBox1.TabIndex = 14;
             // 
             // btnExport
             // 
             this.btnExport.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExport.Location = new System.Drawing.Point(104, 57);
+            this.btnExport.Location = new System.Drawing.Point(208, 110);
+            this.btnExport.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnExport.Name = "btnExport";
-            this.btnExport.Size = new System.Drawing.Size(86, 55);
+            this.btnExport.Size = new System.Drawing.Size(172, 106);
             this.btnExport.TabIndex = 25;
             this.btnExport.Text = "EXPORT";
             this.btnExport.UseVisualStyleBackColor = true;
@@ -168,9 +198,10 @@
             // btnclear
             // 
             this.btnclear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnclear.Location = new System.Drawing.Point(12, 57);
+            this.btnclear.Location = new System.Drawing.Point(24, 110);
+            this.btnclear.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnclear.Name = "btnclear";
-            this.btnclear.Size = new System.Drawing.Size(86, 55);
+            this.btnclear.Size = new System.Drawing.Size(172, 106);
             this.btnclear.TabIndex = 24;
             this.btnclear.Text = "CLEAR";
             this.btnclear.UseVisualStyleBackColor = true;
@@ -179,27 +210,30 @@
             // textBox2
             // 
             this.textBox2.BackColor = System.Drawing.SystemColors.MenuBar;
-            this.textBox2.Location = new System.Drawing.Point(210, 102);
+            this.textBox2.Location = new System.Drawing.Point(420, 196);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(116, 20);
+            this.textBox2.Size = new System.Drawing.Size(228, 31);
             this.textBox2.TabIndex = 27;
             // 
             // textBox3
             // 
             this.textBox3.BackColor = System.Drawing.SystemColors.Menu;
             this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox3.Location = new System.Drawing.Point(405, 4);
+            this.textBox3.Location = new System.Drawing.Point(810, 8);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(40, 13);
+            this.textBox3.Size = new System.Drawing.Size(80, 24);
             this.textBox3.TabIndex = 29;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(464, 4);
+            this.label3.Location = new System.Drawing.Point(928, 8);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(60, 16);
+            this.label3.Size = new System.Drawing.Size(116, 30);
             this.label3.TabIndex = 32;
             this.label3.Text = "Runtime:";
             // 
@@ -207,53 +241,36 @@
             // 
             this.textBox4.BackColor = System.Drawing.SystemColors.MenuBar;
             this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox4.Location = new System.Drawing.Point(527, 4);
+            this.textBox4.Location = new System.Drawing.Point(1054, 8);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(116, 13);
+            this.textBox4.Size = new System.Drawing.Size(232, 24);
             this.textBox4.TabIndex = 31;
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            label4.Location = new System.Drawing.Point(362, 50);
-            label4.Name = "label4";
-            label4.Size = new System.Drawing.Size(106, 20);
-            label4.TabIndex = 34;
-            label4.Text = "Amount Slept";
             // 
             // txtTimeSlep
             // 
             this.txtTimeSlep.BackColor = System.Drawing.SystemColors.Window;
             this.txtTimeSlep.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTimeSlep.Location = new System.Drawing.Point(512, 47);
+            this.txtTimeSlep.Location = new System.Drawing.Point(1024, 90);
+            this.txtTimeSlep.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtTimeSlep.Name = "txtTimeSlep";
-            this.txtTimeSlep.Size = new System.Drawing.Size(116, 26);
+            this.txtTimeSlep.Size = new System.Drawing.Size(228, 44);
             this.txtTimeSlep.TabIndex = 33;
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            label5.Location = new System.Drawing.Point(362, 86);
-            label5.Name = "label5";
-            label5.Size = new System.Drawing.Size(147, 20);
-            label5.TabIndex = 36;
-            label5.Text = "Amount Waking Up";
             // 
             // txtWakingTime
             // 
             this.txtWakingTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtWakingTime.Location = new System.Drawing.Point(512, 84);
+            this.txtWakingTime.Location = new System.Drawing.Point(1024, 162);
+            this.txtWakingTime.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtWakingTime.Name = "txtWakingTime";
-            this.txtWakingTime.Size = new System.Drawing.Size(116, 26);
+            this.txtWakingTime.Size = new System.Drawing.Size(228, 44);
             this.txtWakingTime.TabIndex = 35;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(670, 512);
+            this.ClientSize = new System.Drawing.Size(1340, 985);
             this.Controls.Add(label5);
             this.Controls.Add(this.txtWakingTime);
             this.Controls.Add(label4);
@@ -272,6 +289,7 @@
             this.Controls.Add(this.cmbPorts);
             this.Controls.Add(this.btnConnect);
             this.Controls.Add(this.chart1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Smart Bed Waking Up";
             this.Load += new System.EventHandler(this.Form1_Load);
